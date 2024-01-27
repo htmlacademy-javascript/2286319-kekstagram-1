df2bf35ff748c6d17d1c9d4be8383ba4a5b97849
Keks <keks@htmlacademy.ru>
2024-01-27 23:21:50 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/function.js
@@ -0,0 +1,61 @@
+
+/*Проверка на полиндром*/
+
+// eslint-disable-next-line no-unused-vars
+const isPalindrom = (string) => {
+  const formattedString = string
+    .toLowerCase()
+    .replaceAll(' ', '');
+
+  let reversedString = '';
+  for (let i = formattedString.length - 1; i >= 0; i--) {
+    reversedString += formattedString.at(i);
+  }
+  return formattedString === reversedString;
+};
+/*Функция которая принимает три параметра*/
+
+// eslint-disable-next-line no-unused-vars
+const myPadStart = (string, minLength, pad) => {
+  const currentPad = minLength - string.length;
+
+  if (currentPad <= 0) {
+    return string;
+  }
+
+  return pad.slice(0, currentPad % pad.length) + pad.repeat(currentPad / pad.length) + string;
+};
+/*Длина строки*/
+
+
+// eslint-disable-next-line no-unused-vars
+const checkStringLength = (string, length) =>
+  string.length <= length;
+
+/*Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.*/
+
+// eslint-disable-next-line no-unused-vars
+const getNumber = (string) => {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+};
+
+// eslint-disable-next-line no-console
+console.log(getNumber('2023 год'));
+// eslint-disable-next-line no-console
+console.log(getNumber('ECMAScript 2022'));
+// eslint-disable-next-line no-console
+console.log(myPadStart('1', 2, '0'));
+// eslint-disable-next-line no-console
+console.log(myPadStart('q', 4, 'we'));
+// eslint-disable-next-line no-console
+console.log(isPalindrom('топот'));
+// eslint-disable-next-line no-console
+console.log(isPalindrom('ДовОд'));
+// eslint-disable-next-line no-console
+console.log(checkStringLength('проверяемая строка', 20));
