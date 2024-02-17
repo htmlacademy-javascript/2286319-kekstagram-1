e77c913b2df2b4c146b2f8ac971bb1c7f03d5821
sadieslp <existedat2@gmail.com>
2024-02-18 00:21:18 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 import {getPictures} from './data.js';
-import {renderThumbnails} from '.thumbnail.js';
+import {renderThumbnails} from './thumbnail.js';
 
 renderThumbnails(getPictures());
-// eslint-disable-next-line no-console
-console.log(getPictures());
