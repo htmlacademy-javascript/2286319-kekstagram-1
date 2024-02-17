503018065351d292007dc8fd01eb0e71d5d9e493
Keks <keks@htmlacademy.ru>
2024-02-18 00:28:37 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,4 @@
+import {getPictures} from './data.js';
+import {renderThumbnails} from './thumbnail.js';
+
+renderThumbnails(getPictures());
---
+++ 5/js/util.js
@@ -0,0 +1,21 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (array) =>
+  array[getRandomInteger(0, array.length - 1)];
+
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+const generateCommentId = createIdGenerator();
+
+export {getRandomInteger, getRandomArrayElement, generateCommentId};
