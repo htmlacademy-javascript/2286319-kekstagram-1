e91892d7d4ded103bdb330bda1d6f47e03ee7025
sadieslp <existedat2@gmail.com>
2024-02-18 00:02:43 +0300

    Fixed Const getImages = getPictures

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import {getImages} from './data.js';
 import {renderThumbnails} from '.thumbnail.js';
 
-renderThumbnails(getImages());
+const getPictures = getImages();
+
+renderThumbnails(getPictures());
+// eslint-disable-next-line no-console
+console.log(getImages);
