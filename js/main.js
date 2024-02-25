45a9713fad50a873d1178454481f053a2411551f
sadieslp <existedat2@gmail.com>
2024-02-25 19:39:30 +0300

    -

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import {getPictures} from './data.js';
 import {renderGallery} from './gallery.js';
 
-const generatePictures = getPictures();
-renderGallery(generatePictures);
+renderGallery(getPictures);
