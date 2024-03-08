34ed501554feb87187b135ce3dbfa5e7350e0d8a
sadieslp <existedat2@gmail.com>
2024-03-09 00:56:18 +0300

    -

--- js/gallery.js
+++ js/gallery.js
@@ -9,6 +9,7 @@ const renderGallery = (pictures) => {
     if (!thumbnail) {
       return;
     }
+
     evt.preventDefault();
     const picture = pictures.find(
       (item) => item.id === +thumbnail.dataset.thumbnailId
