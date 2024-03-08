f98d66b6cc4dda0ddfdeed43b6f39ad39481ff2a
sadieslp <existedat2@gmail.com>
2024-03-08 17:46:15 +0300

    module 9

---
+++ js/form.js
@@ -0,0 +1,82 @@
+const MAX_HASHTAG_COUNT = 5;
+const VALID_SYMBOLS = /^#[a-za-яё0-9]{1,19}$/i;
+const TAG_ERROR_TEXT = 'Incorrect Hashtags';
+
+const form = document.querySelector('.img-upload__form');
+const overlay = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const cancelButton = document.querySelector('.#upload-cancel');
+const fileField = document.querySelector('.#upload-file');
+const hashtagField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  ClassTo: 'img-upload__field-wrapper',
+  errorTextParent : 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload_field-wrapper__error',
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  Document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideModal = () => {
+  form.reset();
+  pristine.reser();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+document.activeElement === commentField;
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape' && !isTextFieldFocused()) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+
+const onFileInputChange = () => {
+  showModal();
+};
+
+const isValidTag = (tag) => VALID_SYMBOLS.test(tag);
+
+const hasValidCount = (tags) => tags.lenght <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+const validateTags = (value) => {
+  const tags = value
+    .trim()
+    .split(' ')
+    .filter((tag) => tag.trim().length);
+  return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
+};
+
+pristine.addValidator(
+  hashtagField,
+  validateTags,
+  TAG_ERROR_TEXT
+);
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+fileField.addEventListener('change', onFileInputChange);
+cancelButton.addEventListener('click', onCancelButtonClick);
+form.addEventListener('submit', onFormSubmit);
