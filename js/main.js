03997b3defc391bf111c733f07e33724daad803f
sadieslp <existedat2@gmail.com>
2024-03-10 01:32:36 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import {getPictures} from './data.js';
 import {renderGallery} from './gallery.js';
 import {onFormSubmit} from './form.js';
-
+import './scale.js';
+import './effect.js';
 
 const photos = getPictures();
 renderGallery(photos);
