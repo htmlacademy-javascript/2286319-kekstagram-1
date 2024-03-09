662ed74b470f7008ceb2b0b5af8991604703b543
sadieslp <existedat2@gmail.com>
2024-03-10 02:39:59 +0300

    trying to fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 import {getPictures} from './data.js';
 import {renderGallery} from './gallery.js';
 import './form.js';
-import './scale.js';
-import './effect.js';
-
 
 const photos = getPictures();
 renderGallery(photos);
