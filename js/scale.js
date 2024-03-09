075a9a060a3c7f56a9474eb4087304e1da42d0ff
sadieslp <existedat2@gmail.com>
2024-03-10 01:01:11 +0300

    fixed

--- js/scale.js
+++ js/scale.js
@@ -3,7 +3,7 @@ const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const DEFAULT_SCALE = 100;
 
-const smallerButtonElement = document.querySelector('.scale__control--small');
+const smallerButtonElement = document.querySelector('.scale__control--smaller');
 const biggerButtonElement = document.querySelector('.scale__control--bigger');
 const scaleInputElement = document.querySelector('.scale__control--value');
 const imageElement = document.querySelector('.img-upload__preview img');
