051e2e5527f2fa089562ec43de39498a26d726ec
sadieslp <existedat2@gmail.com>
2024-03-16 13:43:47 +0300

    made new code for message.js

--- js/message.js
+++ js/message.js
@@ -1,55 +1,43 @@
-const body = document.querySelector('body');
-const success = document.querySelector('#success').content.querySelector('.success');
-const error = document.querySelector('#error').content.querySelector('.error');
+const bodyElement = document.querySelector('body');
+const successMessage = document.querySelector('.success');
+const errorMessage = document.querySelector('.error');
 
-const showErrorMessage = () => {
-  const errorElement = error.cloneNode(true);
-  const errorButton = errorElement.querySelector('.error__button');
-  const errorInner = errorElement.querySelector('.error');
-  body.append(errorElement);
-  body.classList.add('error');
-
-  const closeErrorMessage = () => {
-    errorElement.remove();
-    document.removeEventListener('keydown', onDocumentKeydown);
-    body.classList.remove('error');
-  };
-
-  errorButton.addEventListener('click', () => closeErrorMessage());
-  document.addEventListener('click', (evt) => {
-    const click = evt.target.classList.value;
-    if (click === errorInner) {
-      errorElement.remove();
-    }
-  });
-
-  function onDocumentKeydown(evt) {
-    if (evt.key === 'Escape') {
-      closeErrorMessage();
-    }
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    showErrorMessage();
   }
-
-  document.addEventListener('keydown', onDocumentKeydown);
 };
 
-const showSuccessMessage = () => {
-  const successElement = success.cloneNode(true);
-  const closeSuccessButton = successElement.querySelector('.success__button');
-  body.append(successElement);
-
-  const closeSuccesMessage = () => {
-    successElement.remove();
-    document.removeEventListener('keydown', onDocumentKeydown);
-  };
-
-  closeSuccessButton.addEventListener('click', () => closeSuccesMessage());
-
-  function onDocumentKeydown(evt) {
-    if (evt.key === 'Escape') {
-      closeSuccesMessage();
-    }
-  }
+function showSuccessMessage () {
+  bodyElement.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
-};
+}
 
-export {showErrorMessage, showSuccessMessage};
+function showErrorMessage () {
+  bodyElement.classList.add('hidden');
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+successMessage.addEventListener('click', () => {
+  showSuccessMessage();
+});
+
+successMessage.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Enter') {
+    showErrorMessage();
+  }
+});
+
+errorMessage.addEventListener('click', () => {
+  showErrorMessage();
+});
+
+errorMessage.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    showErrorMessage();
+  }
+});
+
+
+export {showSuccessMessage, showErrorMessage};
