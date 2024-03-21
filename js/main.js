6479ab106afbfe8ba6b7633ed64fd85d13445403
sadieslp <existedat2@gmail.com>
2024-03-21 23:00:39 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ try {
   const data = await getData();
   const debouncedRenderGallery = debounce(renderGallery);
   init(data, debouncedRenderGallery);
+  renderGallery(getFilteredPictures);
   getFilteredPictures();
 } catch (err) {
   showAlert(err.message);
